f2fecc10f8b2e72d20a6d312a387489f0ba84052
mariots <mario_ts@outlook.com>
2024-07-21 20:51:43 -0600

    Movies Insfracstructure added

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
-import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   
@@ -8,10 +10,48 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Text(Environment.moviedbKey),
+        child: _HomeView(),
       ),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    return Column(children: [
+      const CustomAppbar(),
+      
+      MoviesSlideshow(movies: nowPlayingMovies)
+
+      // Expanded(child: ListView.builder(
+      //   itemCount: nowPlayingMovies.length,
+      //   itemBuilder: (context, index){
+      //   final movie = nowPlayingMovies[index];
+        
+      //   return ListTile(
+      //    title: Text(movie.title),  
+      //   );
+      // }))
+    ],);
   }
 }
