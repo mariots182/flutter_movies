31dfbc8eb1653d41d9fe78e94422810577c67e97
mariots <mario_ts@outlook.com>
2024-07-22 16:37:12 -0600

    animate_do added

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:card_swiper/card_swiper.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +11,9 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity,
@@ -18,6 +22,12 @@ class MoviesSlideshow extends StatelessWidget {
         viewportFraction: 0.8,
         scale: 0.9,
         autoplay: true,
+        pagination: SwiperPagination(
+          builder: DotSwiperPaginationBuilder(
+            activeColor: colors.primary,
+            color: colors.secondary
+          )
+        ),
         itemBuilder: (context, index) {
           // final movie = movies[index];
           return _Slide(movie:movies[index]);
@@ -53,7 +63,19 @@ class _Slide extends StatelessWidget {
         decoration: decoration ,
         child: ClipRRect(
           borderRadius: BorderRadius.circular(20),
-          child: Image.network(movie.backdropPath,fit: BoxFit.cover,),
+          child: Image.network(
+            movie.backdropPath,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress){
+              if(loadingProgress != null){
+                return const DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.black12),
+                );
+              }
+
+              return FadeIn(child: child);           
+            },
+            ),
           )
       ),
     );
