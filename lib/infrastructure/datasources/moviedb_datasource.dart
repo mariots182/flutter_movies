f44946a13b8237c282b1bd8ebd2523e44e0b5cc0
mariots <mario_ts@outlook.com>
2024-07-28 17:47:17 -0600

    upcoming, top_rated & popular catefories added

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -13,11 +13,8 @@ class MoviesDbDatasource extends MoviesDatasource {
         'language': Environment.moviedbLanguage
       }));
 
-  @override
-  Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response =
-        await dio.get('/movie/now_playing', queryParameters: {'page': page});
-    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+  List<Movie> _jsonToMovies(Map<String, dynamic> json) {
+    final movieDBResponse = MovieDbResponse.fromJson(json);
 
     final List<Movie> movies = movieDBResponse.results
         .where((moviedb) => moviedb.posterPath != 'no-poster')
@@ -25,5 +22,37 @@ class MoviesDbDatasource extends MoviesDatasource {
         .toList();
 
     return movies;
+  }
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response =
+        await dio.get('/movie/now_playing', queryParameters: {'page': page});
+
+    return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<List<Movie>> getPopular({int page = 1}) async {
+    final response =
+        await dio.get('/movie/popular', queryParameters: {'page': page});
+
+    return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<List<Movie>> getUpcoming({int page = 1}) async {
+    final response =
+        await dio.get('/movie/upcoming', queryParameters: {'page': page});
+
+    return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<List<Movie>> getTopRated({int page = 1}) async {
+    final response =
+        await dio.get('/movie/top_rated', queryParameters: {'page': page});
+
+    return _jsonToMovies(response.data);
   }
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -30,11 +30,17 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   void initState() {
     super.initState();
     ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
   }
 
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
+    final topRatedMovies = ref.watch(topRatedMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     return CustomScrollView(slivers: [
@@ -59,54 +65,45 @@ class _HomeViewState extends ConsumerState<_HomeView> {
           ),
 
           MovieHorizontalListview(
-            movies: nowPlayingMovies,
-            title: "En cines",
-            subtitle: "Lunes 20",
-            loadNextPage: () {
-              ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-            },
-          ),
-
-          MovieHorizontalListview(
-            movies: nowPlayingMovies,
+            movies: upcomingMovies,
             title: "Proximamente",
             subtitle: "En este mes",
             loadNextPage: () {
-              ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+              ref.read(upcomingMoviesProvider.notifier).loadNextPage();
             },
           ),
 
           MovieHorizontalListview(
-            movies: nowPlayingMovies,
+            movies: popularMovies,
             title: "Populares",
             loadNextPage: () {
-              ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+              ref.read(popularMoviesProvider.notifier).loadNextPage();
             },
           ),
 
           MovieHorizontalListview(
-            movies: nowPlayingMovies,
+            movies: topRatedMovies,
             title: "Mejor calificadas",
             subtitle: "Siempre",
             loadNextPage: () {
-              ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+              ref.read(topRatedMoviesProvider.notifier).loadNextPage();
             },
           ),
 
           const SizedBox(
             height: 10,
           )
+
           // Expanded(child: ListView.builder(
           //   itemCount: nowPlayingMovies.length,
           //   itemBuilder: (context, index){
           //   final movie = nowPlayingMovies[index];
-
           //   return ListTile(
           //    title: Text(movie.title),
           //   );
           // }))
         ]);
-      }, childCount: 10))
+      }, childCount: 1))
     ]);
   }
 }
